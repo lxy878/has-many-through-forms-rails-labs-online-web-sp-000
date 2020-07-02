7af3b8b9af2d49bd4ba9bf8c01f33aa7772fedb7
Xiaoyi Liu <lxy878@gmail.com>
2020-07-02 12:04:55 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user_attributes=(name)
+    self.user = User.find_or_create_by(name) if name[:username].present?
+  end
 end
